055566443a2d01a396294bbecd90ac723a99e779
aliyanwhiz <aliyanahmed853@gmail.com>
2026-04-04 23:30:09 +0500

    first commit

---
+++ CP_A04_Task1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "My Name is Ahmed" << endl;
+    cout << "I am learning C++" << endl;
+    cout << "This is my first program" << endl;
+    return 0;
+}
---
+++ CP_A04_Task4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name\tAge\tCity" << endl;
+    cout << "Ali\t19\tKarachi" << endl;
+    cout << "Sara\t18\tLahore" << endl;
+    return 0;
+}
