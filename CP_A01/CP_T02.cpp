f63386234a5bdee4639beff1445fff51668d6b80
aliyanwhiz <aliyanahmed853@gmail.com>
2026-04-05 00:02:18 +0500

    Indexing Updated

---
+++ CP_A01/CP_T02.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: Aliyan Ahmed.\nRoll No: SP26-BSCS-0144\nDepartment: Computer Science" << endl;
+    return 0;
+}
---
+++ CP_A02/CP_T01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Printing 'I' and 'T' using their respective characters
+    cout << "IIIII  TTTTT" << endl;
+    cout << "  I      T  " << endl;
+    cout << "  I      T  " << endl;
+    cout << "  I      T  " << endl;
+    cout << "IIIII    T  " << endl;
+    return 0;
+}
