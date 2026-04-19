c41df28442a16dc6fe4d2b3dd6da9d3e2a18c3b6
aliyanwhiz <aliyanahmed853@gmail.com>
2026-04-19 22:50:43 +0500

    Taylor series Completed

--- CP_Prac/Taylor_Exp.cpp
+++ CP_Prac/Taylor_Exp.cpp
@@ -2,23 +2,31 @@
 using namespace std;
 
 int main() {
-    double x, n, result = 1;
+    double x, n, poweredBase,Factorial;
+    double res = 1;
     cout << "Enter x: ";
     cin >> x;
     cout << "Enter n: ";
     cin >> n;
 
-    int numrator , denominator = 0;
-    // int result = 1;
-    for (int i = 1; i < n; i++) {
-        for (int j = 1; j <= i; j++) {
-            numrator = x *= i;
-            denominator *= i;
-            result += numrator / denominator;
+    for(int i = 1; i <= n; i++)
+    {
+        // moving to new term with inital input
+        poweredBase = x;
+        Factorial = 1;
+        for(int j = 1; j <= i; j++)
+        {
+            //power setting
+            if(j > 1) poweredBase *= x;
+
+            //factorial setting
+            Factorial *= j;
         }
+
+        // cout << i << " Pwr : " << poweredBase << " | Fac : " << Factorial << endl;
+        res += (poweredBase/Factorial);
     }
 
-
-    cout << "Result: " << result << endl;
+    cout << "Result: " << res << endl;
     return 0;
 }
